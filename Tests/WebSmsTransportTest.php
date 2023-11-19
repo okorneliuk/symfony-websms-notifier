e1b3e70e4ce93d2638ba7be5ff966f906b21b8c6
Oleh Korneliuk <oleh.korneliuk@gmail.com>
2023-11-19 21:38:47 +0200

    fixed tests

--- Tests/WebSmsTransportTest.php
+++ Tests/WebSmsTransportTest.php
@@ -14,6 +14,7 @@ use Symfony\Component\Notifier\Message\ChatMessage;
 use Symfony\Component\Notifier\Message\MessageInterface;
 use Symfony\Component\Notifier\Message\SmsMessage;
 use Symfony\Component\Notifier\Test\TransportTestCase;
+use Symfony\Component\Notifier\Tests\Transport\DummyMessage;
 use Symfony\Component\Notifier\Transport\TransportInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -22,24 +23,24 @@ final class WebSmsTransportTest extends TransportTestCase
     /**
      * @return WebSmsTransport
      */
-    public function createTransport(?HttpClientInterface $client = null): TransportInterface
+    public static function createTransport(?HttpClientInterface $client = null): TransportInterface
     {
-        return new WebSmsTransport('uid', 'api_key', true, $client ?? $this->createMock(HttpClientInterface::class));
+        return new WebSmsTransport('uid', 'api_key', true, $client ?? new MockHttpClient());
     }
 
-    public function toStringProvider(): iterable
+    public static function toStringProvider(): iterable
     {
-        yield ['websms://api.websms.com?test_mode=1', $this->createTransport()];
+        yield ['websms://api.websms.com?test_mode=1', self::createTransport()];
     }
 
-    public function supportedMessagesProvider(): iterable
+    public static function supportedMessagesProvider(): iterable
     {
         yield [new SmsMessage('0611223344', 'Hello!')];
     }
 
-    public function unsupportedMessagesProvider(): iterable
+    public static function unsupportedMessagesProvider(): iterable
     {
         yield [new ChatMessage('Hello!')];
-        yield [$this->createMock(MessageInterface::class)];
+        yield [new DummyMessage()];
     }
 }
